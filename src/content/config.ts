2c05a6ef0fb9a74916560b0c52efd8dae2c5297c
Sil <silveltman@gmail.com>
2024-07-08 13:41:05 +0200

    fix table imports and updates fulldev-ui

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,84 @@
-import { defineCollection, reference } from 'astro:content'
-import { blockSchema, pageSchema, pathSchema } from 'fulldev-ui'
+// import { defineCollection, reference, z, type AnyEntryMap } from 'astro:content'
 
-const references = {
-  page: pathSchema.pipe(reference('pages')),
-  pages: pathSchema.pipe(reference('pages')).array(),
-}
+// const pathSchema = <C extends keyof AnyEntryMap>(collection: C) =>
+//   z
+//     .string()
+//     .transform((value) => {
+//       const fullpath = value.split(`${collection}/`).pop()
+//       const slug = fullpath?.split('.').shift()
+//       return slug
+//     })
+//     .pipe(reference(collection))
 
-const block = blockSchema.extend(references).partial().passthrough()
+// const references = {
+//   page: pathSchema('pages'),
+//   pages: pathSchema('pages').array(),
+// }
 
-const page = pageSchema
-  .extend(references)
-  .extend({
-    hero: block,
-    blocks: block.array(),
-  })
-  .partial()
-  .passthrough()
+// export const buttonSchema = z
+//   .object({
+//     text: z.string(),
+//     html: z.string(),
+//     href: z.string(),
+//   })
+//   .partial()
+//   .passthrough()
 
-export const collections = {
-  pages: defineCollection({
-    type: 'content',
-    schema: page,
-  }),
-}
+// const baseSchema = z
+//   .object({
+//     image: z.string(),
+//     images: z.string().array(),
+//     icon: z.string(),
+//     rating: z.number(),
+//     badge: z.string(),
+//     tagline: z.string(),
+//     heading: z.string(),
+//     text: z.string(),
+//     list: z.string().array(),
+//     price: z.string().or(z.number()),
+//     button: buttonSchema,
+//     buttons: buttonSchema.array(),
+//   })
+//   .partial()
+//   .passthrough()
+
+// export const blockSchema = baseSchema
+//   .extend({
+//     card: baseSchema,
+//     cards: baseSchema.array(),
+//   })
+//   .extend(references)
+//   .partial()
+//   .passthrough()
+
+// export const pageSchema = baseSchema
+//   .extend({
+//     title: z.string(),
+//     description: z.string(),
+//     head: z
+//       .object({
+//         title: z.string(),
+//         description: z.string(),
+//         image: z.string().optional(),
+//       })
+//       .partial(),
+//   })
+//   .extend(references)
+//   .partial()
+//   .passthrough()
+
+// const page = pageSchema
+//   .extend(references)
+//   .extend({
+//     hero: blockSchema,
+//     blocks: blockSchema.array(),
+//   })
+//   .partial()
+//   .passthrough()
+
+// export const collections = {
+//   pages: defineCollection({
+//     type: 'content',
+//     schema: page,
+//   }),
+// }
