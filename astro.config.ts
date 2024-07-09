33f32349715d7da5865238e9fcd18bdcbcb2b11b
Sil <silveltman@gmail.com>
2024-07-09 09:38:32 +0200

    Content bookshop with preview

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import react from '@astrojs/react'
 import bookshop from '@bookshop/astro-bookshop'
+import yaml from '@rollup/plugin-yaml'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
@@ -8,4 +9,7 @@ export default defineConfig({
   output: 'static',
   site: 'https://example.com',
   integrations: [UnoCSS(), react(), bookshop()],
+  vite: {
+    plugins: [yaml()],
+  },
 })
