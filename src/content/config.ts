65409337ffb3c21c123f46d663eb5e39bc36be23
Sil <silveltman@gmail.com>
2024-07-11 14:32:46 +0200

    config.ts simplified

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,80 +1,24 @@
-import { defineCollection, reference, z, type AnyEntryMap } from 'astro:content'
-
-const pathSchema = <C extends keyof AnyEntryMap>(collection: C) =>
-  z
-    .string()
-    .transform((value) => {
-      const fullpath = value.split(`${collection}/`).pop()
-      const slug = fullpath?.split('.').shift()
-      return slug
-    })
-    .pipe(reference(collection))
+import { defineCollection, z } from 'astro:content'
+import { pageSchema, pathSchema } from 'fulldev-blocks'
 
 const references = {
   page: pathSchema('pages'),
   pages: pathSchema('pages').array(),
+  category: pathSchema('categories'),
+  categories: pathSchema('categories').array(),
+  product: pathSchema('products'),
+  products: pathSchema('products').array(),
 }
 
-export const buttonSchema = z
-  .object({
-    text: z.string(),
-    html: z.string(),
-    href: z.string(),
-  })
-  .partial()
-  .passthrough()
-
-const baseSchema = z
-  .object({
-    image: z.string(),
-    images: z.string().array(),
-    icon: z.string(),
-    rating: z.number(),
-    badge: z.string(),
-    tagline: z.string(),
-    heading: z.string(),
-    text: z.string(),
-    list: z.string().array(),
-    price: z.string().or(z.number()),
-    button: buttonSchema,
-    buttons: buttonSchema.array(),
-  })
-  .partial()
-  .passthrough()
-
-export const blockSchema = baseSchema
-  .extend({
-    card: baseSchema,
-    cards: baseSchema.array(),
-  })
-  .extend(references)
-  .partial()
-  .passthrough()
-
-export const pageSchema = baseSchema
-  .extend({
-    title: z.string(),
-    description: z.string(),
-    hero: blockSchema,
-    content: blockSchema,
-    blocks: blockSchema.array(),
-    head: z
-      .object({
-        title: z.string(),
-        description: z.string(),
-        image: z.string().optional(),
-      })
-      .partial(),
-  })
-  .extend(references)
-  .partial()
-  .passthrough()
+const collection = defineCollection({
+  type: 'content',
+  schema: pageSchema(references),
+})
 
 export const collections = {
-  pages: defineCollection({
-    type: 'content',
-    schema: pageSchema,
-  }),
+  pages: collection,
+  categories: collection,
+  products: collection,
   globals: defineCollection({
     type: 'data',
     schema: z.any(),
