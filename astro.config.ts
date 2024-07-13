6e70f3c9b29e683fbc1b0f68137977e6df3ed911
Sil <silveltman@gmail.com>
2024-07-13 09:48:30 +0200

    bookshop integration added back

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,13 @@
+import bookshop from '@bookshop/astro-bookshop'
 import yaml from '@rollup/plugin-yaml'
 import { defineConfig } from 'astro/config'
 import fulldevBlocks from 'fulldev-blocks/integration'
 import UnoCSS from 'unocss/astro'
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://example.com',
-  integrations: [UnoCSS(), fulldevBlocks()],
+  integrations: [UnoCSS(), fulldevBlocks(), bookshop()],
   vite: {
     plugins: [yaml() as any],
   },
--- src/content/pages/contact.md
+++ src/content/pages/contact.md
@@ -1,9 +1,3 @@
 ---
 _schema: default
-blocks:
-  - _bookshop_name: Contact
-    heading: Heb je vragen?
-    text: We nemen binnen 1 werkdag persoonlijk contact met je op.
-  - _bookshop_name: Hero
-    heading: Hello world
 ---
