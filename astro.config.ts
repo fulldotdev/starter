8d9710d84ffe1aaa7ae02d0e19dd28878efb1721
Sil <silveltman@gmail.com>
2024-07-13 10:50:23 +0200

    .17

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,9 @@
-import bookshop from '@bookshop/astro-bookshop'
-import yaml from '@rollup/plugin-yaml'
 import { defineConfig } from 'astro/config'
 import fulldevBlocks from 'fulldev-blocks/integration'
 import UnoCSS from 'unocss/astro'
-// https://astro.build/config
+
 export default defineConfig({
   output: 'static',
   site: 'https://example.com',
-  integrations: [UnoCSS(), fulldevBlocks(), bookshop()],
-  vite: {
-    plugins: [yaml() as any],
-  },
+  integrations: [UnoCSS(), fulldevBlocks()],
 })
